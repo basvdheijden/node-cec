6c23dc452c5e58b693cfbd60f957e1f985bb0a10
Bas van der Heijden <bas.van.der.heijden@dop.nu>
2013-06-13 10:31:42 +0200

    Moved CEC client from lib to index

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
-var exec = require('child_process').exec,
-    CEC = require('./lib/cec');
+var Stream = require('stream'),
+  Event = require('events').EventEmitter,
+  exec = require('child_process').exec,
 
-var child = exec('cec-client');
-var cec = new CEC(child.stdout);
 
-cec.event.on('key', function(k) {
-  console.log(k);
-});
+var CEC = function(inputStream) {
+  var self = this;
+
+  if (!inputStream) {
+    var child = exec('cec-client');
+    inputStream = child.stdout;
+  }
+
+  this.inputStream = inputStream;
+
+  this.stream = new Stream();
+  this.stream.writable = true;
+
+  this.event = new Event();
+
+  this.stream.write = function(buffer) {
+    var data = buffer.toString();
+    var matches = data.match(/key pressed: ([^\s]+)/);
+    if (matches && matches.length > 1) {
+      self.event.emit('key', matches[1]);
+    }
+  };
+
+  this.stream.end = function(buffer) {
+    if (buffer) {
+      this.write(buffer);
+    }
+
+    this.writable = false;
+  };
+
+  this.inputStream.pipe(this.stream);
+
+  return this;
+};
+
+module.exports = exports = CEC;
